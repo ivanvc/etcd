98f0867ca3e19bf7e87ebb8f1d4fb9e4be95964a
Ivan Valdes <ivan@vald.es>
2025-12-18 16:59:25 -0800

    debug
    Signed-off-by: Ivan Valdes <ivan@vald.es>

--- scripts/test_lib.sh
+++ scripts/test_lib.sh
@@ -325,10 +325,11 @@ function go_test {
     # shellcheck disable=SC2086
     additional_flags=$(${flags_for_package_func} ${pkg})
 
-    set -x
     # shellcheck disable=SC2206
+    set -x
     local cmd=( go test ${goTestFlags} ${additional_flags} ${pkg} "$@" )
     set +x
+    continue
 
     # shellcheck disable=SC2086
     if ! run env ${goTestEnv} ETCD_VERIFY="${ETCD_VERIFY}" "${cmd[@]}" | tee ${junit_filename_prefix:+"${junit_filename_prefix}.stdout"} | grep --binary-files=text "${go_test_grep_pattern}" ; then
@@ -419,6 +420,7 @@ function run_go_tests {
     set -x
     local cmd=(go test "${go_test_flags[@]}" "${pkg}" "${args[@]}")
     set +x
+    continue
 
     local junit_filename_prefix
     junit_filename_prefix=$(get_junit_filename_prefix "${junit_report_dir}")
