797a6ab59a1c60baf93d6b1c0ec43a006eaef3ca
Ivan Valdes <ivan@vald.es>
2026-02-05 22:24:10 -0800

    Use Docker buildx to build multi-arch images
    Replace building individual images and then a manifest from the
    individual images, with Docker buildx passing all the supported
    platforms.
    
    This will also allow to later push the Docker images from our CI/CD
    pipelines.
    
    Signed-off-by: Ivan Valdes <ivan@vald.es>

--- scripts/release.sh
+++ scripts/release.sh
@@ -244,7 +244,19 @@ main() {
     log_warning "Skipping release build step. /release directory already exists."
   else
     log_callout "Building release..."
-    REPOSITORY=$(pwd) ./scripts/build-release.sh "${RELEASE_VERSION}"
+
+    if [ "${DRY_RUN}" == "true" ] || [ "${NO_DOCKER_PUSH}" == 1 ]; then
+      log_callout "Skipping docker push. --no-docker-push flag is set."
+      # Explicitly set NO_DOCKER_PUSH to 1, if DRY_RUN is true.
+      NO_DOCKER_PUSH=1
+    else
+      read -p "Publish etcd ${RELEASE_VERSION} docker images to registries [y/N]? " -r confirm
+      if [[ "${confirm,,}" == "y" ]]; then
+        NO_DOCKER_PUSH=1
+      fi
+    fi
+
+    REPOSITORY=$(pwd) ./scripts/build-release.sh "${RELEASE_VERSION}" "${NO_DOCKER_PUSH}"
   fi
 
   # Sanity checks.
@@ -277,50 +289,16 @@ main() {
     maybe_run gsutil -m acl ch -u allUsers:R -r "gs://etcd/${RELEASE_VERSION}/"
   fi
 
-  # Push images.
-  if [ "${DRY_RUN}" == "true" ] || [ "${NO_DOCKER_PUSH}" == 1 ]; then
-    log_callout "Skipping docker push. --no-docker-push flag is set."
-  else
-    read -p "Publish etcd ${RELEASE_VERSION} docker images to quay.io [y/N]? " -r confirm
-    [[ "${confirm,,}" == "y" ]] || exit 1
-    # shellcheck disable=SC2034
-    for i in {1..5}; do
-      docker login quay.io && break
-      log_warning "login failed, retrying"
-    done
-
-    for TARGET_ARCH in "amd64" "arm64" "ppc64le" "s390x"; do
-      log_callout "Pushing container images to quay.io ${RELEASE_VERSION}-${TARGET_ARCH}"
-      maybe_run docker push "quay.io/coreos/etcd:${RELEASE_VERSION}-${TARGET_ARCH}"
-      log_callout "Pushing container images to gcr.io ${RELEASE_VERSION}-${TARGET_ARCH}"
-      maybe_run docker push "gcr.io/etcd-development/etcd:${RELEASE_VERSION}-${TARGET_ARCH}"
-    done
-
-    log_callout "Creating manifest-list (multi-image)..."
-
-    for TARGET_ARCH in "amd64" "arm64" "ppc64le" "s390x"; do
-      maybe_run docker manifest create --amend "quay.io/coreos/etcd:${RELEASE_VERSION}" "quay.io/coreos/etcd:${RELEASE_VERSION}-${TARGET_ARCH}"
-      maybe_run docker manifest annotate "quay.io/coreos/etcd:${RELEASE_VERSION}" "quay.io/coreos/etcd:${RELEASE_VERSION}-${TARGET_ARCH}" --arch "${TARGET_ARCH}"
-
-      maybe_run docker manifest create --amend "gcr.io/etcd-development/etcd:${RELEASE_VERSION}" "gcr.io/etcd-development/etcd:${RELEASE_VERSION}-${TARGET_ARCH}"
-      maybe_run docker manifest annotate "gcr.io/etcd-development/etcd:${RELEASE_VERSION}" "gcr.io/etcd-development/etcd:${RELEASE_VERSION}-${TARGET_ARCH}" --arch "${TARGET_ARCH}"
-    done
-
-    log_callout "Pushing container manifest list to quay.io ${RELEASE_VERSION}"
-    maybe_run docker manifest push "quay.io/coreos/etcd:${RELEASE_VERSION}"
-
-    log_callout "Pushing container manifest list to gcr.io ${RELEASE_VERSION}"
-    maybe_run docker manifest push "gcr.io/etcd-development/etcd:${RELEASE_VERSION}"
-  fi
-
   ### Release validation
   mkdir -p downloads
 
   # Check image versions
-  for IMAGE in "quay.io/coreos/etcd:${RELEASE_VERSION}" "gcr.io/etcd-development/etcd:${RELEASE_VERSION}"; do
-    if [ "${DRY_RUN}" == "true" ] || [ "${NO_DOCKER_PUSH}" == 1 ]; then
-      IMAGE="${IMAGE}-amd64"
-    fi
+  local images=("quay.io/coreos/etcd:${RELEASE_VERSION}" "gcr.io/etcd-development/etcd:${RELEASE_VERSION}")
+  if [ -n "${REGISTRY:-}" ]; then
+    images=("${REGISTRY}/etcd:${VERSION}")
+  fi
+
+  for IMAGE in "${images[@]}"; do
     # shellcheck disable=SC2155
     local image_version=$(docker run --rm "${IMAGE}" etcd --version | grep "etcd Version" | awk -F: '{print $2}' | tr -d '[:space:]')
     if [ "${image_version}" != "${VERSION}" ]; then
